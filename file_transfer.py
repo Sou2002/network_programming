bec227e6f4e0e0daac422ca32c0f129e3c153a2b
Sourav Banerjee <souravbnjee07@gmail.com>
2024-09-19 02:20:15 +0530

    Added initial FileTransfer class and some methods (empty)

---
+++ file_transfer.py
@@ -0,0 +1,24 @@
+import os
+import socket
+
+
+
+# A class to transfer files
+class FileTransfer:
+    '''
+    '''
+
+    def __init__(self) -> None:
+        '''
+        '''
+        pass
+
+    def send(self):
+        '''
+        '''
+        pass
+
+    def receive(self):
+        '''
+        '''
+        pass
