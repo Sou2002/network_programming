41288a96351a08fefcd6d12fb4ab0a97287e3ea4
Sourav Banerjee <souravbnjee07@gmail.com>
2024-09-19 13:00:26 +0530

    Built send method

--- file_transfer.py
+++ file_transfer.py
@@ -2,7 +2,6 @@ import os
 import socket
 
 
-
 # A class to transfer files
 class FileTransfer:
     '''
@@ -11,14 +10,41 @@ class FileTransfer:
     def __init__(self) -> None:
         '''
         '''
+        self.__PORT: int = 8000  # Port Number
+
+    # A method to send file
+    def send(self, file_path: str, HOST: str = "localhost") -> None:
+        '''
+        '''
+        # Creating a sender socket
+        # socket.AF_INET = Internet IPv4
+        # socket.SOCK_STREAM = TCP Connection
+        __sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connecting with receiver
+        __sender_socket.connect((HOST, self.__PORT))
+
+        # Reading the file
+        __file = open(file_path, 'rb')
+        __file_size: int = os.path.getsize(file_path)
+
+        try:
+            __sent_size: int = __sender_socket.sendfile(__file)
+
+            if __file_size == __sent_size:
+                print("File sent successfully!")
+                print(f"File size = {format(__sent_size / (2 ** 20), '.2f')} MB")
+
+        except socket.error as err:
+            print(err)
+
+        __file.close()
+        __sender_socket.close()
+
+    def receive(self) -> None:
+        '''
+        '''
         pass
 
-    def send(self):
-        '''
-        '''
-        pass
 
-    def receive(self):
-        '''
-        '''
-        pass
+if __name__ == '__main__':
+    pass
