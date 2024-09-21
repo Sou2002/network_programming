5e4e3212f1c8362faca80d37dc74f0d0a308478b
Sourav Banerjee <souravbnjee07@gmail.com>
2024-09-21 14:51:34 +0530

    Created constructor to set up the server and broadcast method

--- tcp_chat_room/chat_server.py
+++ tcp_chat_room/chat_server.py
@@ -7,16 +7,26 @@ class ChatServer:
     '''
     '''
 
-    def __init__(self) -> None:
+    def __init__(self, ip_address: str = "localhost", port_number: int = 8001) -> None:
         '''
         '''
-        pass
+        self.IP_ADDRESS: str = ip_address  # Host IP Address
+        self.PORT: int = port_number  # Port Number
+
+        # Creating the server socket
+        # socket.AF_INET = Internet IPv4
+        # socket.SOCK_STREAM = TCP Connection
+        self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server.bind((self.IP_ADDRESS, self.PORT))
+
+        self.__clients: dict = []
 
     # A method to brodcast message
-    def __broadcast(self) -> None:
+    def __broadcast(self, message: bytes) -> None:
         '''
         '''
-        pass
+        for nickname, socket in self.__clients:
+            socket.send(message)
 
     # A method to receive message
     def __receive(self) -> None:
