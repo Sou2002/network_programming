d3fea339e21addb4b5a5b475672aa43ef4ab2631
Sourav Banerjee <souravbnjee07@gmail.com>
2024-09-22 13:29:05 +0530

    Added boiler plate code for chat_client

---
+++ tcp_chat_room/chat_client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+
+# Chat Client Class
+class ChatClient:
+    '''
+    '''
+    def __init__(self) -> None:
+        '''
+        '''
+        pass
+
+    # A method to recieve message from server
+    def __recieve(self) -> None:
+        '''
+        '''
+        pass
+
+    # A method to send message to server
+    def __send(self) -> None:
+        '''
+        '''
+        pass
+
+    def start_client(self) -> None:
+        '''
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    pass
