77266244ec21e4abfc666554a75573aef2f7219d
Sourav Banerjee <souravbnjee07@gmail.com>
2024-09-22 13:43:55 +0530

    Added constructor to connect with the server

--- tcp_chat_room/chat_client.py
+++ tcp_chat_room/chat_client.py
@@ -6,10 +6,16 @@ import threading
 class ChatClient:
     '''
     '''
-    def __init__(self) -> None:
+    def __init__(self, ip_address: str = "localhost", port_number: int = 8001) -> None:
         '''
         '''
-        pass
+        # Creating the server socket
+        # socket.AF_INET = Internet IPv4
+        # socket.SOCK_STREAM = TCP Connection
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__client.connect((ip_address, port_number))
+
+        self.__username: str = input("Enter a username:")
 
     # A method to recieve message from server
     def __recieve(self) -> None:
